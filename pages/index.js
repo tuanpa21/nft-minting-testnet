2bce707724d816c70d16f642ad667644ceffa259
Pham Anh Tuan <phamtuan210394@gmail.com>
2022-05-09 11:54:25 +0700

    Fix navigate wrong route

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   const router = useRouter();
 
     useEffect(() => {
-        if (isAuthenticated) router.push("/dashboard");
+        if (isAuthenticated) router.push("/upload").then();
     }, [isAuthenticated]);
 
   return (
